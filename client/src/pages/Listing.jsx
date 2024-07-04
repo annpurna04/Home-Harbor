ec519539fe3383301cfe3bcf5c3af9837cdcaa9a
annpura04 <annpurnagupta040401@gmail.com>
2024-07-04 14:26:05 +0530

    adding image slider to the llisting page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -40,8 +40,8 @@ export default function Listing() {
     {listing && !loading && !error && <div>
     <Swiper navigation>
         {listing.imageUrls.map(url=>
-        <SwiperSlide key={url}>
-            <div className='h-[550px]' style={{background:`url(${url}) center no-repeat`, backgroundSize: 'cover'}}></div>
+        <SwiperSlide key={url}>  
+            <div className='h-[550px]' style={{background:`url(${url}) center no-repeat`, backgroundSize: `url(${url}) center no-repeat ` , backgroundSize:'cover'}}></div>
         </SwiperSlide>
         )}
         </Swiper>
