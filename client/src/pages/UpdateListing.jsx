340cc4691befcc97f4f41a80030e7cce0e984045
annpura04 <annpurnagupta040401@gmail.com>
2024-07-20 16:34:47 +0530

    create the listing item components and show listings

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -227,7 +227,7 @@ const handleSubmit=async (e)=> {
                       </div>
                     ))
                  }
-                 <button disabled={loading || uploading } className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Creating...':'Update listing'}</button>
+                 <button disabled={loading || uploading } className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">{loading ? 'Updating...':'Update listing'}</button>
             {error && <p className="text-red-700 text-sm">{error}</p>}
             </div>
         </form>
