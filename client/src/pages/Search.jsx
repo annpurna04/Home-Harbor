92bfbef54f37655c64f69fc197224534b5f43ce4
annpura04 <annpurnagupta040401@gmail.com>
2024-07-24 12:54:53 +0530

    add showmore listing functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -13,6 +13,7 @@ export default function Search() {
     });
     const [loading,setLoading] = useState(false);
     const [listings,setListings] = useState([]);
+    const [showMore, setShowMore] =useState(false);
     
     useEffect(() =>{
         const urlParams = new URLSearchParams(location.search);
@@ -39,9 +40,15 @@ export default function Search() {
         }
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if(data.length > 8){
+                setShowMore(true);
+            }else{
+                setShowMore(false); 
+            }
             setListings(data);
             setLoading(false);
         }
@@ -80,6 +87,19 @@ urlParams.set('order',sidebardata.order);
 const searchQuery = urlParams.toString();
 navigate(`/search?${searchQuery}`);
 
+};
+const onShowMoreClick = async () => {
+   const numberOfListings = listings.length; 
+   const startIndex = numberOfListings;
+   const urlParams = new URLSearchParams(location.search);
+   urlParams.set('startIndex',startIndex);
+   const searchQuery = urlParams.toString();
+   const res = await fetch(`/api/listing/get?${searchQuery}`);
+   const data= await res.json();
+   if(data.length <9 ){
+    setShowMore(false);
+   }
+   setListings([...listings,...data]);
 }
 
   return (
@@ -178,8 +198,15 @@ navigate(`/search?${searchQuery}`);
         {
             !loading && listings && listings.map((listing) =>(
                 <ListingItem key={listing._id} listing={listing}/> 
-            ))
-        }
+            ))}
+            {showMore &&(
+                <button
+                onClick={onShowMoreClick} 
+                className='text-green-700 hover:underline p-7 text-center w-full'  
+                >
+                    Show more
+                    </button>
+            )}
     </div>
         </div>
     </div>
