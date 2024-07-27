b74ff57e471294bc7b245f50ef5e1d6f9bd4489e
annpura04 <annpurnagupta040401@gmail.com>
2024-07-27 11:43:35 +0530

    about page completed

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Home Harbor</h1>
+      <p className='mb-4 text-slate-700'>Home Harbor is a place where your dream house meets its ideal harbor. We are dedicated to creating extraordinary living spaces that combine luxury, comfort, and community, all tucked away in a peaceful and welcoming setting. We think that your house is a haven where you can truly live and prosper, not just a place to live.</p>
+      <p className='mb-4 text-slate-700'>Experience the ultimate coastal lifestyle at Home Harbor. Wake up to the sound of waves, enjoy leisurely strolls along pristine beaches, and indulge in a vibrant community. Our homes are designed to enhance your well-being and create lasting memories.</p>
+      <p className='mb-4 text-slate-700'>Home Harbor offers an unparalleled living experience with its prime beachfront location, cutting-edge amenities, and eco-friendly design. Our commitment to excellence is evident in every detail, from the finest materials to the impeccable craftsmanship.</p>
+    </div>
   )
 }
